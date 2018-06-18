374eff97351a2af1265ea29942c557ceb2bfa5c0
cmoutella <emaildacarolmoutella@gmail.com>
2018-06-18 16:45:22 -0300

    entrada de novos projetos

---
+++ JAVA/Registradora/src/MainAPS.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author email
+ */
+
+import Interface.JFrame_PDV;
+public class MainAPS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame_PDV pdv = new JFrame_PDV();
+        pdv.setTitle("PDV - Ponto de Venda");
+        pdv.setLocationRelativeTo(null);
+        pdv.setVisible(true);
+    }
+    
+}
---
+++ Web/TheCode/README.md
@@ -0,0 +1,14 @@
+﻿The Code
+------
+PT
+The Code é um projeto para aprendizado rápido de desenvolvimento web. Utilizando HTML5, CSS3 e Bootstrap.
+Esse arquivo deve ficar em constante modificação para aperfeiçoamento das técnicas aprendidas.
+
+O objetivo deste exercício é desenvolver um formulário.
+------
+EN
+
+The Code is a project for fast-learning in web development. Using HTML5, CSS3 and Bootstrap.
+This folder's project shall be in constant update, to forshow the improviment of skills learned.
+
+The object of this exercise is develop a form.
